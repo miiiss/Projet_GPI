138f9400bf67a60d3d1f1d7f487bbc75220ac653
Valeur-Nes <valeuraucibi@gmail.com>
2016-04-02 15:04:16 +0200

    Voilà je viens de faire une grande modif et un bon menu

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!-- Page d'accueil du site évènementiel Fun mouvement -->
+
+<html>
+<head>
+	<!-- Cette balise sert à indiquer au navigateur d'afficher les accens des lettres -->
+	<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="Styles/style_PA.css" /><!-- Appel du fichier CSS principal -->
+		<title>Fun mouvement</title>
+</head>
+<body>
+
+	<p><h1>Fun mouvement</h1></p><!-- Titre de la page -->
+
+		<div class="nav"><!-- Ce block ne contient que le menu de la page -->
+			<ul class="navigation">
+				<li><a href="#" class="text_grand">Accueil</a></li><!-- Accueil -->
+				<li><a href="#" class="text_grand">S'incrire</a></li><!-- S'inscrire -->
+				<li><a href="#" class="text_grand">Évènements</a>
+					<ul>
+						<li><a href="#">Pour les jeunes</a></li>
+						<li><a href="#">Pour les adultes</a></li>
+					</ul>
+				</li><!-- Évènement -->
+				<li><a href="#" class="text_grand">Se connecter</a></li><!-- Se connecter -->
+				<li><a href="#" class="text_grand">Contacter</a></li><!-- Contacte -->
+			</ul>
+		</div>
+
+	<p><hr></p> <!-- Ligne de séparation entre le menu et le slogan du site -->
+		<p class="slogan">S'amuser partout en France</p>
+
+	<!-- image au centre de la page -->
+	<p><img src="Images/PA1.jpg" class="image"></p>
+
+	<!-- footer contenant quelques information -->
+	<footer>
+		<p class="text_footer">Tout droits réservés  |  Université Paris-Sud  |  Iut d'Orsay</p>
+			<p class="style_nom">André  |  Aucibi  |  Ebeyer  |  Zhang</p>
+	</footer>
+
+</body>
+</html>
